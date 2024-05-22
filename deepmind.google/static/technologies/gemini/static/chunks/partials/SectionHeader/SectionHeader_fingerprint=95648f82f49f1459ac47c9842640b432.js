b7100735ac3e9e85a1be15cb80f5582c939c7dbf
Eduardo-Fidelis <emaildescartaveldoedu@gmail.com>
2024-05-21 21:42:22 -0300

    Add files via upload

---
+++ deepmind.google/static/technologies/gemini/static/chunks/partials/SectionHeader/SectionHeader_fingerprint=95648f82f49f1459ac47c9842640b432.js
@@ -0,0 +1,1 @@
+import{a as d}from"../../chunks/chunk-PEATFMNR-min.js";import"../../chunks/chunk-P56TXV5W-min.js";import{a as o}from"../../chunks/chunk-PVKMAFVQ-min.js";import"../../chunks/chunk-E27WR637-min.js";var n=({partial:e})=>o("div",{id:e==null?void 0:e.id,className:"section-header","data-section":(e==null?void 0:e.sectionId)||!1},e.header&&o(d,{...e.header})),c=n;export{c as default};
